31dacdd0d2578bd9a290d37c4291499ecd20c3f1
uharston <uharston@gmail.com>
2020-06-16 11:10:03 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,17 @@
+class PigLatinizer 
+
+    attr_reader :string
+
+    @@all = [] 
+
+    def initialize(string) 
+        @string = string 
+        @@all << self 
+    end 
+
+    def convert 
+
+    end 
+
+
+end 
